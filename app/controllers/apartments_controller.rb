1f2d3c90a2385ed042540d987432dc199e595865
placidowang <placidow@gmail.com>
2020-02-20 00:35:31 -0600

    built most things

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,55 @@
 class ApartmentsController < ApplicationController
   # add controller methods
+  set :views, "app/views/apartments"
+
+  get "/apartments" do
+    @apartments = Apartment.all
+    erb :index
+  end
+
+  get "/apartments/new" do
+    erb :new
+  end
+
+  get "/apartments/:id" do
+    @apartment = current_apartment
+    # @tenants = Tenant.all.select {|tenant| tenant.apartment_id == @apartment.id}
+    erb :show
+  end
+
+  get "/apartments/:id/edit" do
+    @apartment = current_apartment
+    # binding.pry
+    erb :edit
+  end
+
+  # post "/apartments/:id" do
+    
+  # end
+
+
+
+
+
+  post "/apartments" do
+    a = Apartment.create(params)
+    redirect "/apartments/#{a.id}"
+  end
+
+  delete "/apartments/:id" do
+    current_apartment.destroy
+    redirect "/apartments"
+  end
+
+
+  patch "/apartments/:id" do
+    # author = Author.find(params[:id])
+    # author.update(name: params[:name])
+    current_apartment.update(address: params[:address])
+    redirect "/apartments"
+  end
+  
+  def current_apartment
+    Apartment.find(params[:id])
+  end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,3 +1,52 @@
 class TenantsController < ApplicationController
   # add controller methods
+  set :views, "app/views/tenants"
+
+  get "/tenants" do
+    @tenants = Tenant.all
+    erb :index
+  end
+
+  # You should be able to associate an apartment
+  # when creating a new tenant
+  get "/tenants/new" do
+    @apartments = Apartment.all
+    erb :new
+  end
+
+  get "/tenants/:id" do
+    @tenant = current_tenant
+    erb :show
+  end
+
+  # You should be able to associate an apartment
+  # when editing a tenant
+  get "/tenants/:id/edit" do
+    @tenant = current_tenant
+    @apartments = Apartment.all
+    # binding.pry
+    erb :edit
+  end
+
+  post "/tenants" do
+    t = Tenant.create(params)
+    redirect "/tenants/#{t.id}"
+    # redirect "/tenants"
+  end
+
+  delete "/tenants/:id" do
+    current_tenant.destroy
+    redirect "/tenants"
+  end
+
+  patch "/tenants/:id" do
+    # author = Author.find(params[:id])
+    # author.update(name: params[:name])
+    current_tenant.update(name: params[:name], apartment_id: params[:apartment_id])
+    redirect "/tenants"
+  end
+  
+  def current_tenant
+    Tenant.find(params[:id])
+  end
 end
